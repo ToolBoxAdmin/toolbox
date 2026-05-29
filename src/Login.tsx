440df8cc7fe48c45fcffcc90d55aa8623902500d
ToolBoxAdmin <mdlfuente2002@gmail.com>
2026-05-29 15:07:05 -0600

    Bug Issue

--- src/Login.tsx
+++ src/Login.tsx
@@ -20,7 +20,7 @@ export default function Login() {
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ email, password }),
+          body: JSON.stringify({ username, password }),
         }
       );
 
